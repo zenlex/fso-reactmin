4c8ae8800c68297a9cd869ff0ce42161c78e6736
zenlex <erichrkeil@gmail.com>
2022-03-05 18:20:16 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>
+        press
+      </button>
+      <div>
+        {values}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'core-js/stable/index'
+import 'regenerator-runtime/runtime'
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'))
